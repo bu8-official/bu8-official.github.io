5ef0870ed17b29edfcdf127805da71971cf2454a
bu8-official <110025564+bu8-official@users.noreply.github.com>
2024-10-19 05:49:09 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Sample JavaScript (optional) - Could handle the progress bars or interactive elements.
-
-const files = document.querySelectorAll('.file button');
-
-files.forEach(file => {
-    file.addEventListener('click', () => {
-        alert('Download initiated for ' + file.previousElementSibling.textContent);
-    });
-});
